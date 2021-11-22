67e624b6758fad2e3a2e5cda0b9d8b2cf3e82e5d
nguyen912 <nguyenhanguyen912@gmail.com>
2021-11-22 17:39:31 +0700

    day8 nov 22 2021

--- Day8/caesar_cipher/main.py
+++ Day8/caesar_cipher/main.py
@@ -1,40 +1,46 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-go_again = True
-
-
-def execute_program(direction_input, text_input, shift_input):
-    result = ""
-    if direction_input == "encode":
-        for letter in text_input:
-            if letter in alphabet:
-                result += alphabet[alphabet.index(letter) - shift_input]
-            else:
-                result += letter
-        print("Here's the encoded result: " + result)
-    elif direction_input == "decode":
-        for letter in text_input:
-            if letter in alphabet:
-                shift_position = alphabet.index(letter) + shift_input
-                if shift_position < len(alphabet):
-                    result += alphabet[alphabet.index(letter) + shift_input]
-                else:
-                    final_shift_position = (len(alphabet) - shift_position) * -1
-                    result += alphabet[final_shift_position]
-            else:
-                result += letter
-        print("Here's the decoded result: " + result)
-    go_again = input("Type 'yes' if you want to go again. Otherwise, type 'no'.")
-    if go_again == 'yes':
-        go_again = True
-    elif go_again == 'no':
-        go_again = False
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d code: {end_text}")
 
 
-while go_again:
-    execute_program(direction, text, shift)
+print(logo)
+
+restart = True
+while restart:
+    direction = ""
+    correct_format = False
+    while not correct_format:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        if direction == "encode" or direction == "decode":
+            correct_format = True
+        else:
+            print("Please type again!")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift %= 26
+
+    caesar(text, shift, direction)
+
+    ask_continue = input("Type 'yes' to try again. Otherwise, type 'no'.\n")
+    if ask_continue == "no":
+        restart = False
+        print("Goodbye.")
+
+
