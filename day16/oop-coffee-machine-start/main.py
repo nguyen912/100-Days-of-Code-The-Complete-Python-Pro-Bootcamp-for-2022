6463b34203299a48745dcceecba15a8416e49721
nguyen912 <nguyenhanguyen912@gmail.com>
2022-06-22 18:04:36 +0700

    day 16

--- day16/oop-coffee-machine-start/main.py
+++ day16/oop-coffee-machine-start/main.py
@@ -1,3 +1,18 @@
-from menu import Menu, MenuItem
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+from classes.menu import Menu, MenuItem
+from classes.coffee_maker import CoffeeMaker
+from classes.money_machine import MoneyMachine
+
+
+def order_drink():
+    menu = Menu()
+
+    drink = input(f'What would you like? ({menu.get_items()}): ')
+    if menu.find_drink(drink) is not None:
+        print(f'{drink} is being dispensed')
+    else:
+        order_drink()
+
+
+if __name__ == '__main__':
+    order_drink()
+
