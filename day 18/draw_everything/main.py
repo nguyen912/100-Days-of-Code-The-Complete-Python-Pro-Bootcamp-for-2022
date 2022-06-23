6c0ab3abd963f7f370a155b7049cead9db4d041c
nguyen912 <nguyenhanguyen912@gmail.com>
2022-06-23 22:43:38 +0700

    day 18 square

--- day 18/draw_everything/main.py
+++ day 18/draw_everything/main.py
@@ -1,7 +1,7 @@
-#####Turtle Intro######
-
 import turtle as t
+
 if __name__ == '__main__':
+    # turtle
     timmy_the_turtle = t.Turtle()
     timmy_the_turtle.shape("turtle")
     timmy_the_turtle.color("red")
@@ -11,4 +11,21 @@ if __name__ == '__main__':
     timmy_the_turtle.left(180)
     timmy_the_turtle.setheading(0)
 
-######## Challenge 1 - Draw a Square ############
+    # draw a square
+    square = t.Turtle()
+    square.forward(100)
+    square.right(90)
+    square.forward(100)
+    square.left(90)
+    square.backward(100)
+    square.right(90)
+    square.backward(100)
+
+    # another way to draw square
+    new_square = t.Turtle()
+    for i in range(4):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.left(90)
+
+    #
+
